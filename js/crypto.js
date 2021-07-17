b8b59d7d2c23e0d6cc16814af1b0efdc59f7e11d
Cathy Slider <cslider1@aol.com>
2021-07-17 13:03:09 -0400

    email validate and start form save

--- js/crypto.js
+++ js/crypto.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 // Add Countdown Timer
-var countDownDate = new Date("Oct 31, 2021 15:37:25").getTime();
+var countDownDate = new Date("Oct 31, 2021 13:00:37").getTime();
 
 var x = setInterval(function () {
   var now = new Date().getTime();
@@ -32,33 +32,91 @@ var x = setInterval(function () {
   }
 }, 1000);
 
-async function getCoinGecko() {
-  let url =
-    "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd";
-  try {
-    let res = await fetch(url);
-    return await res.json();
-  } catch (error) {
-    console.log(error);
-  }
+// Fetch Dog Breeds
+const CoinGecko = require("coingecko-api");
+const CoinGeckoClient = new CoinGecko();
+
+var func = async () => {
+  let data = await CoinGeckoClient.coins.all();
+};
+
+const select = document.getElementById("coins");
+// const card = document.querySelector(".card");
+
+// const select = document.getElementById("breeds");
+// const card = document.querySelector(".card");
+// const form = document.querySelector("form");
+
+// ------------------------------------------
+//  FETCH FUNCTIONS
+// ------------------------------------------
+
+function fetchData(url) {
+  return fetch(url).then((res) => res.json());
 }
 
-async function renderCoinGecko() {
-  let prices = await getCoinGecko();
-  let html = "";
-  prices.forEach((price) => {
-    let htmlSegment = `<div class="price">
-                        <h3>${price.bitcoin}</h3>
-                        </div>`;
-    html += htmlSegment;
+fetchData(
+  "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc"
+).then((data) => generateOptions(data.message));
+
+// fetchData("https://dog.ceo/api/breeds/image/random").then((data) =>
+//   generateImage(data.message)
+// );
+
+// ------------------------------------------
+//  HELPER FUNCTIONS
+// ------------------------------------------
+
+function generateOptions(data) {
+  const options = data
+    .map(
+      (item) => `
+    <option value='${item}'>${item}</option>
+  `
+    )
+    .join("");
+  select.innerHTML = options;
+}
+
+function generateImage(data) {
+  const html = `
+    <img src='${data}' alt>
+    <p>Click to view images of ${select.value}s</p>
+  `;
+  card.innerHTML = html;
+}
+
+function fetchBreedImage() {
+  const breed = select.value;
+  const img = card.querySelector("img");
+  const p = card.querySelector("p");
+
+  fetchData(`https://dog.ceo/api/breed/${breed}/images/random`).then((data) => {
+    img.src = data.message;
+    img.alt = breed;
+    p.textContent = `Click to view more ${breed}s`;
   });
 }
 
-renderCoinGecko();
+// ------------------------------------------
+//  EVENT LISTENERS
+// ------------------------------------------
+select.addEventListener("change", fetchBreedImage);
+card.addEventListener("click", fetchBreedImage);
 
-// //Fetch CoinGecko API
-// let coingecko =
-//   "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd";
+// ------------------------------------------
+//  POST DATA
+// ------------------------------------------
 
-// fetch (coingecko);
-//   .then();
+// Validate Email with RegEx
+function ValidateEmail(inputText) {
+  var mailformat =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  if (inputText.value.match(mailformat)) {
+    // alert("Valid email address!");
+    return true;
+  } else {
+    alert("Invalid email address!");
+    return false;
+  }
+}
