b5ea0c16e42c0618bd26668711fe032e72b20494
Cathy Slider <cslider1@aol.com>
2021-07-17 15:07:47 -0400

    flex rearrange

--- js/crypto.js
+++ js/crypto.js
@@ -113,7 +113,7 @@ function ValidateEmail(inputText) {
   var mailformat =
     /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
   if (inputText.value.match(mailformat)) {
-    // alert("Valid email address!");
+    alert("Thank you!");
     return true;
   } else {
     alert("Invalid email address!");
